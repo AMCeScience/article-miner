c7bad68afaf4f97d7f76a6fd732d344cfa48e8fb
Flythe <Flythe1@gmail.com>
2015-09-08 14:29:43 +0200

    addslashes on keywords

--- models/alchemy_outcomes.php
+++ models/alchemy_outcomes.php
@@ -2,11 +2,15 @@
 
 class AlchemyOutcomes {
   function insert_keyword($db, $keyword, $outcome_id) {
-    $db->query("INSERT INTO Alchemy_keywords (text, relevance, outcome) VALUES ('{$keyword["text"]}', '{$keyword["relevance"]}', '$outcome_id')");
+    $fixed = addslashes($keyword["text"]);
+
+    $db->query("INSERT INTO Alchemy_keywords (text, relevance, outcome) VALUES ('$fixed', '{$keyword["relevance"]}', '$outcome_id')");
   }
 
   function insert_concept($db, $concept, $outcome_id) {
-    $db->query("INSERT INTO Alchemy_concepts (text, relevance, outcome) VALUES ('{$concept["text"]}', '{$concept["relevance"]}', '$outcome_id')");
+    $fixed = addslashes($concept["text"]);
+
+    $db->query("INSERT INTO Alchemy_concepts (text, relevance, outcome) VALUES ('$fixed', '{$concept["relevance"]}', '$outcome_id')");
   }
 
   function insert_taxonomy($db, $taxonomy, $outcome_id) {
@@ -16,11 +20,15 @@ class AlchemyOutcomes {
       $confident = $taxonomy["confident"];
     }
 
-    $db->query("INSERT INTO Alchemy_taxonomy (label, score, confident, outcome) VALUES ('{$taxonomy["label"]}', '{$taxonomy["score"]}', '$confident', '$outcome_id')");
+    $fixed = addslashes($taxonomy["label"]);
+
+    $db->query("INSERT INTO Alchemy_taxonomy (label, score, confident, outcome) VALUES ('$fixed', '{$taxonomy["score"]}', '$confident', '$outcome_id')");
   }
 
   function insert_entity($db, $entity, $outcome_id) {
-    $db->query("INSERT INTO Alchemy_entities (type, relevance, count, text, outcome) VALUES ('{$entity["type"]}', '{$entity["relevance"]}', '{$entity["count"]}', '{$entity["text"]}', '$outcome_id')");
+    $fixed = addslashes($entity["type"]);
+
+    $db->query("INSERT INTO Alchemy_entities (type, relevance, count, text, outcome) VALUES ('$fixed', '{$entity["relevance"]}', '{$entity["count"]}', '{$entity["text"]}', '$outcome_id')");
   }
 
   function clear($db) {
